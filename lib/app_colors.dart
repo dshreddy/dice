ffceaccd9d4f3a020cd20b90f8c1bf5853c2f56e
dshreddy03 <112101014@smail.iitpkd.ac.in>
2024-01-03 18:21:36 +0530

    version 0

---
+++ lib/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkPrimaryColor = Color(0xFFD32F2F);
+  static const Color lightPrimaryColor = Color(0xFFFFCDD2);
+  static const Color primaryColor = Color(0xFFF44336);
+  static const Color textIconsColor = Colors.white;
+  static const Color accentColor = Color(0xFFFF5722);
+  static const Color primaryTextColor = Color(0xFF212121);
+  static const Color secondaryTextColor = Color(0xFF757575);
+  static const Color dividerColor = Color(0xFFBDBDBD);
+}
